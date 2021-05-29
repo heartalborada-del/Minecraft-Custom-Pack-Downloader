be7950bc12dd08bdb409087ee9007215f7eb20db
heartalborada <heartalborada@foxmail.com>
2021-05-29 20:58:43 +0800

    更新网络检测

--- src/main/java/me/heartalborada/downloader/Internet.java
+++ src/main/java/me/heartalborada/downloader/Internet.java
@@ -1,18 +1,26 @@
 package me.heartalborada.downloader;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Internet {
     public static boolean Ping(String internet_url) {
-        URL url = null;
-        try {
-            url = new URL(internet_url);
-            InputStream in = url.openStream();
-            in.close();
-            return true;
-        } catch (IOException e) {
+        {
+            HttpURLConnection conn;
+            try {
+                URL url = new URL(internet_url);
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setConnectTimeout(15000);
+                conn.setReadTimeout(60000);
+                conn.setRequestProperty("Accept", "application/json");
+                conn.connect();
+                int Code=conn.getResponseCode();
+                if (Code!=404) {
+                    return true;
+                }
+            } catch (IOException ignored) { }
             return false;
         }
     }
